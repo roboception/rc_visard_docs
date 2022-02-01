3a26017aa3b5e4761154fa26da4e5674274773dd
Roboception GmbH <support@roboception.de>
2022-02-01 11:34:59 +0000

    generated docs from CI

--- v22.01/de/_static/documentation_options.js
+++ v22.01/de/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '22.01.4',
+    VERSION: '22.01.4-2-g9ad05af',
     LANGUAGE: 'de',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
